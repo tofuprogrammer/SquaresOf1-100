fff7b9bd94ae297472cf4199687f3ce9890bef5f
Sam Hart <81168131+tofuprogrammer@users.noreply.github.com>
2024-12-26 20:15:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# Required for program to wait
+import time
+# Introduction
+print("Squares of 1-100")
+# Wait for 3 seconds before listing squares of 1-100
+time.sleep(3)
+# The for loop
+for i in range (1, 101):
+    print(i ** 2)
